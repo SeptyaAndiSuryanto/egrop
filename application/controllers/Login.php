3392d927b6bcf65277bdc478acd07ee97faffedd
SeptyaAndiSuryanto <septyaandi24@gmail.com>
2019-04-27 16:47:46 +0700

    added AdminLTE

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,1 +1,16 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function index()
+    {
+        $faker = Faker\Factory::create('id_ID');
+        for($i=0; $i<20; $i++){
+            echo $faker->name.'<br';
+        }
+    }
+
+}
+
+/* End of file Controllername.php */
