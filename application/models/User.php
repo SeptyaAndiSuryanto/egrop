e8a74d99895dcc924c27bbcb064ec9b3f26ad73f
SeptyaAndiSuryanto <septyaandi24@gmail.com>
2019-04-29 20:43:22 +0700

    add model user

---
+++ application/models/User.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Model {
+    function logged_id(){
+        return $this->session->userdata('user_id');
+        
+    }
+    
+    function check_login($table, $field1, $field2){
+        $this->db->select('*');
+        $this->db->from('$table');
+        $this->db->where($field1);
+        $this->db->where($field2);
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if($query->num_rows() == 0){
+            return FALSE;
+        }else{
+            return $query->result();
+        }
+                
+    }
+
+}
+
+/* End of file ModelName.php */
