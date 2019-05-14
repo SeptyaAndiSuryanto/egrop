4d44bcf2b642398d1a791c9eec71640270d91a11
SeptyaAndiSuryanto <septyaandi24@gmail.com>
2019-05-14 21:34:55 +0700

    after template

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,10 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-
+    
+    public function __construct()
+    {
+        parent::__construct();
+        if($this->session->userdata('logged_in') !== TRUE){
+            redirect('login');
+        }
+    }
+    
     public function index()
     {
         $this->load->view('_template/head');
+        $this->load->view('_template/breadcumbs');
+        $this->load->view('_template/nav');
+        $this->load->view('_template/content');
+        $this->load->view('_template/footer');
     }
 
 }
